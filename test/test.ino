1f6048e36b7e65926274dfd33679c74c25acaff4
ishino <nonchan95425@gmail.com>
2019-07-08 17:45:40 +0900

    interrupt

--- test/test.ino
+++ test/test.ino
@@ -1,19 +1,39 @@
-#define 
+#define SW PB3
+#define LED PB4
 
 void setup()
 {
-    pinMode(2, OUTPUT);
+    DDRB = (1 << LED);   //LEDを出力
+    PORTB = ~(1 << LED); //LEDをLOW その他をプルアップ
+
+    //interrupt settings
+    GIMSK = 0x20; //PCIE -> 1
+    PCMSK = 0x8;  //PCINT3 -> 1
+    sei();
+}
+
+ISR(PCINT0_vect)
+{
+    PINB = (1 << LED);
 }
 
 void loop()
 {
-
-    if (digitalRead(3) == LOW)
+    /*
+    if (!(PINB & _BV(SW)))
     {
-        digitalWrite(2, HIGH);
+        PORTB |= (1 << LED);
     }
     else
     {
-        digitalWrite(2, LOW);
-    }
+        PORTB &= ~(1 << LED);
+    } 
+    */
+
+    //PINB = (1 << LED);
+    _delay_ms(100);
+}
+
+void delay_us(short us)
+{
 }
