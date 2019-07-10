2ef4d7d4e79b9e5e410b53557b4f8982028d81a2
ishino <nonchan95425@gmail.com>
2019-07-10 21:40:52 +0900

    pwm ok

--- test/test.ino
+++ test/test.ino
@@ -1,47 +1,65 @@
 #include <avr/sleep.h>
+
 #define SW PB3
 #define LED PB4
 
 void setup()
 {
+    //pin settings
     DDRB = (1 << LED);   //LEDを出力
-    PORTB = ~(1 << LED); //LEDをLOW その他をプルアップ
+    PORTB = ~(1 << LED); //LEDをLOWに その他をプルアップ(SW)
 
     //interrupt settings
-    GIMSK = 0x20; //PCIE -> 1
-    PCMSK = 0x8;  //PCINT3 -> 1
-    sei();
+    GIMSK = 0x20; //PCIE -> enable
+    PCMSK = 0x8;  //PCINT3 -> set interrupt pin
+    sei();        //whole permmit interrupt
+
+    TCNT1 = 0;
 }
 
-ISR(PCINT0_vect)
-{
-}
+
+
 
 void loop()
 {
+    //sleep settings
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     sleep_enable();
     sleep_cpu();
     sleep_disable();
 
-    //PORTB |= (_BV(LED));
-    _delay_ms(3000);
-    //PORTB &= ~(_BV(LED));
-
-    /*
-    if (!(PINB & _BV(SW)))
+    //main process
+    _delay_ms(10);         //チャタリング対策
+    if (!(PINB & _BV(SW))) //ピン変化割り込み時,押下のみ実行
     {
-        PORTB |= (1 << LED);
+        //memo 押しっぱなしでループ＆信号は１回だけ出すようにする
+        pwmOn();
+        _delay_us(200);
+        pwmOff();
+        _delay_us(200);
+        pwmOn();
+        _delay_us(200);
+        pwmOff();
+
     }
-    else
-    {
-        PORTB &= ~(1 << LED);
-    } 
-    */
-
-    //PINB = (1 << LED);
 }
 
-void delay_us(short us)
+void pwmOn()
+{
+    PORTB |= _BV(LED);
+    TCCR1 = 0b10000011;
+    GTCCR = 0b01100000;
+    OCR1C = 54;
+    OCR1B = 12;
+}
+
+void pwmOff()
+{
+    PORTB &= ~_BV(LED);
+    GTCCR = 0;
+    TCNT1 = 0;
+}
+
+ISR(PCINT0_vect)
 {
 }
