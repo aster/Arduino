721bf2c6ed477649c9ae47f39849656167880baa
ishino <nonchan95425@gmail.com>
2019-07-11 17:49:20 +0900

    beta

--- roomRemote/roomRemote.ino
+++ roomRemote/roomRemote.ino
@@ -1,11 +1,14 @@
+#include <TinyWireM.h>
+#include <USI_TWI_Master.h>
 #include <avr/sleep.h>
 
-#define F_CPU 1000000UL
+#define Model "ATtiny85"
 #define SW PB3
 #define LED PB4
 
 void setup()
 {
+
     //pin settings
     DDRB = (1 << LED);   //LEDを出力
     PORTB = ~(1 << LED); //LEDをLOWに その他をプルアップ(SW)
@@ -16,10 +19,15 @@ void setup()
     sei();        //whole permmit interrupt
 
     TCNT1 = 0;
+
+    TinyWireM.begin(); // i2cの初期化、マスターとする
+    delay(1000);
 }
 
 long onSignal = 8061411525;
 long offSignal = 8347845;
+short margin_us = 40;
+short margin_delay = 0;
 
 void loop()
 {
@@ -30,44 +38,55 @@ void loop()
     sleep_disable();
 
     //main process
-    _delay_ms(10);         //チャタリング対策
+    delay(10);             //チャタリング対策
     if (!(PINB & _BV(SW))) //ピン変化割り込み時,押下のみ実行
     {
-        //memo 押しっぱなしでループ＆信号は１回だけ出すようにする
-        delay(1000);
-        lightSet(1);
-        delay(1000);
+        //lightSet(1);
+        //delay(2000);
         lightSet(0);
+        delay(2000);
     }
+
+    //i2c serial print
+    static int num = 0;
+    char sbuf[40];
+
+    sprintf(sbuf, "TinyWireM num=%d model=%s\n", num, Model);
+    num++;
+    i2c_print(sbuf);
 }
 
 void startSignal()
 {
     //3.2ms -> on  and  1.6ms -> off
     pwmOn();
-    _delay_us(3200);
+    my_delay_us(3200);
     pwmOff();
-    _delay_us(1600);
+    my_delay_us(1550);
 }
 
 void signalA()
 {
     //0.4ms -> on  and  1.2ms -> off
     pwmOn();
-    _delay_us(400);
+    //delayMicroseconds(400 + margin_delay);
+    my_delay_us(350);
 
     pwmOff();
-    _delay_us(1200);
+    //delayMicroseconds(1227 - margin_us);
+    my_delay_us(1150);
 }
 
 void signalB()
 {
     //0.4ms -> on  and  0.4ms -> off
     pwmOn();
-    _delay_us(400);
+    //delayMicroseconds(400 + margin_delay);
+    my_delay_us(350);
 
     pwmOff();
-    _delay_us(400);
+    //delayMicroseconds(429 - margin_us);
+    my_delay_us(350);
 }
 
 //light on  onflag == 1
@@ -92,10 +111,10 @@ void lightSet(short onFlag)
 void pwmOn()
 {
     PORTB |= _BV(LED);
-    TCCR1 = 0b10000001;
+    TCCR1 = 0b10000100;
     GTCCR = 0b01100000;
     OCR1C = 25;
-    OCR1B = 6;
+    OCR1B = 7;
 }
 
 void pwmOff()
@@ -105,6 +124,40 @@ void pwmOff()
     TCNT1 = 0;
 }
 
+//1MHz -> 64us 分解能
+void my_delay_us(short d_time)
+{
+    short start_time = micros();
+    short now_time = start_time;
+
+    while (now_time - start_time < d_time){
+        now_time = micros();
+    }
+    start_time = now_time = 0;
+
+}
+
 ISR(PCINT0_vect)
 {
+}
+
+// 最大10文字にデータを分割してデータの送信
+void i2c_print(char *str)
+{
+    int pnum;
+    int jmax;
+    pnum = (strlen(str) + 9) / 10; //パケット数
+    for (int i = 0; i < pnum; i++)
+    {
+        jmax = i * 10 + 10;
+        if (strlen(str) < jmax)
+            jmax = strlen(str);
+        TinyWireM.beginTransmission(8); // 通信の開始処理、スレーブのアドレスは８とする
+        for (int j = i * 10; j < jmax; j++)
+        {                           // 1パケットの最大文字数は10文字
+            TinyWireM.send(str[j]); // 通信データ送信
+        }
+        TinyWireM.send(0); // 通信データ送信(終端文字)
+        TinyWireM.endTransmission();
+    }
 }
